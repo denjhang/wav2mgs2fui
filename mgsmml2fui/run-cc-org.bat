85ad27f0cf1ca641b58e923d0f1fcce09e333267
Denjhang Liu <61811583+denjhang@users.noreply.github.com>
2025-02-10 12:18:28 +0800

    bat

--- mgsmml2fui/run-cc-org.bat
+++ mgsmml2fui/run-cc-org.bat
@@ -1,16 +1,5 @@
-echo off
-for /f "tokens=1 delims=." %%a in ('dir *.txt /b') do (call:Func %%a) 
-call move.bat
+set name=%~n1
+echo %name%.txt
+cc %name%.txt %name%\
 pause
 
-
-
-:Func
-echo %1.txt
-
-cc %1.txt %1\
-copy %1.txt %1\
-copy %1.dat %1\
-del %1.txt
-del %1.dat
-goto:eof
